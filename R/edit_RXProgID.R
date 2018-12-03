48e8e9deef46a476af71e84a783fbdb32f85bf31
ResourceX <mseman@resourcex.net>
2018-12-03 18:59:45 -0500

    updates

--- R/edit_RXProgID.R
+++ R/edit_RXProgID.R
@@ -16,64 +16,64 @@
 
 edit_RXProgID<-function(RX_ProgID,	ItemMeta1=NULL,	ServiceType=NULL,	ProgName=NULL,	ProgDescription=NULL){
 
+    if(!is.null(RX_ProgID)){
+        db_host<-'ec2-52-11-250-69.us-west-2.compute.amazonaws.com'
+        db_name<-'RX_Admin'
 
-    db_host<-'ec2-52-11-250-69.us-west-2.compute.amazonaws.com'
-    db_name<-'RX_Admin'
-
-    con <- dbConnect(MySQL(),
-                       user="mtseman",
-                       password="cree1234",
-                       host=db_host,
-                       dbname=db_name)
-
-
-
-        if (!is.null(ItemMeta1)){
-
-          ItemMeta1<-trim(ItemMeta1)
-          ItemMeta1<-gsub("'","''",ItemMeta1)
-
-          statement<-paste0("UPDATE RX_ProgInfo SET ItemMeta1 = '",ItemMeta1,"' WHERE RX_ProgID = ",RX_ProgID,";")
-          rs<-dbSendQuery(con,statement)
-          dbClearResult(rs)
-
-        }
-
-        if (!is.null(ServiceType)){
-
-          ServiceType<-trim(ServiceType)
-          ServiceType<-gsub("'","''",ServiceType)
-
-          statement<-paste0("UPDATE RX_ProgInfo SET ServiceType = '",ServiceType,"' WHERE RX_ProgID = ",RX_ProgID,";")
-          rs<-dbSendQuery(con,statement)
-          dbClearResult(rs)
-
-        }
-
-        if (!is.null(ProgName)){
-
-          ProgName<-trim(ProgName)
-          ProgName<-gsub("'","''",ProgName)
-
-          statement<-paste0("UPDATE RX_ProgInfo SET ProgName = '",ProgName,"' WHERE RX_ProgID = ",RX_ProgID,";")
-          rs<-dbSendQuery(con,statement)
-          dbClearResult(rs)
-
-        }
-
-        if (!is.null(ProgDescription)){
-
-          ProgDescription<-trim(ProgDescription)
-          ProgDescription<-gsub("'","''",ProgDescription)
-
-          statement<-paste0("UPDATE RX_ProgInfo SET ProgDescription = '",ProgDescription,"' WHERE RX_ProgID = ",RX_ProgID,";")
-          rs<-dbSendQuery(con,statement)
-          dbClearResult(rs)
-
-        }
+        con <- dbConnect(MySQL(),
+                           user="mtseman",
+                           password="cree1234",
+                           host=db_host,
+                           dbname=db_name)
 
 
 
-    dbDisconnect(con)
+            if (!is.null(ItemMeta1)){
+
+              ItemMeta1<-trim(ItemMeta1)
+              ItemMeta1<-gsub("'","''",ItemMeta1)
+
+              statement<-paste0("UPDATE RX_ProgInfo SET ItemMeta1 = '",ItemMeta1,"' WHERE RX_ProgID = ",RX_ProgID,";")
+              rs<-dbSendQuery(con,statement)
+              dbClearResult(rs)
+
+            }
+
+            if (!is.null(ServiceType)){
+
+              ServiceType<-trim(ServiceType)
+              ServiceType<-gsub("'","''",ServiceType)
+
+              statement<-paste0("UPDATE RX_ProgInfo SET ServiceType = '",ServiceType,"' WHERE RX_ProgID = ",RX_ProgID,";")
+              rs<-dbSendQuery(con,statement)
+              dbClearResult(rs)
+
+            }
+
+            if (!is.null(ProgName)){
+
+              ProgName<-trim(ProgName)
+              ProgName<-gsub("'","''",ProgName)
+
+              statement<-paste0("UPDATE RX_ProgInfo SET ProgName = '",ProgName,"' WHERE RX_ProgID = ",RX_ProgID,";")
+              rs<-dbSendQuery(con,statement)
+              dbClearResult(rs)
+
+            }
+
+            if (!is.null(ProgDescription)){
+
+              ProgDescription<-trim(ProgDescription)
+              ProgDescription<-gsub("'","''",ProgDescription)
+
+              statement<-paste0("UPDATE RX_ProgInfo SET ProgDescription = '",ProgDescription,"' WHERE RX_ProgID = ",RX_ProgID,";")
+              rs<-dbSendQuery(con,statement)
+              dbClearResult(rs)
+
+            }
+
+          dbDisconnect(con)
+
+    }
 
 }
